2d94c2c66852ff1c6d9445de26ad522639425785
Erik Olofsson <erik.olofsson@hansoft.com>
2016-08-23 18:34:29 +0000

    [Malterlib/Core] Fix windows compile error
    Copied from Perforce
     Change: 71046

--- Source/Dummy/Dummy.cpp
+++ Source/Dummy/Dummy.cpp
@@ -1,4 +1,6 @@
 ﻿// Copyright © 2015 Hansoft AB 
 // Distributed under the MIT license, see license text in LICENSE.Malterlib
 
+#ifndef DPlatformFamily_Windows
 int g_DummySholudNotBeLinked = 0;
+#endif
