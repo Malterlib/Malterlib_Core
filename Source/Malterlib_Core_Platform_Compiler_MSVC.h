732502a7119828e502b7dbc49e0e587bf22a5d05
Erik Olofsson <erik@olofsson.info>
2020-06-04 17:27:34 +0200

    [Malterlib/Core] Update to Visual Studio 16.6

--- Source/Malterlib_Core_Platform_Compiler_MSVC.h
+++ Source/Malterlib_Core_Platform_Compiler_MSVC.h
@@ -5,10 +5,10 @@
 
 #if defined(__EDG__)
 // This is the intellisense compiler
-#elif _MSC_VER == 1925
-#	if _MSC_FULL_VER < 192528611
+#elif _MSC_VER == 1926
+#	if _MSC_FULL_VER < 192628806
 #		pragma message("Current compiler version: " DMibStringize(_MSC_FULL_VER))
-#		error "Compiler version is not newer than 192528611, please install the approprita service pack of Visual Studio 2019"
+#		error "Compiler version is not newer than 192628806, please install the approprita service pack of Visual Studio 2019"
 #	endif
 #else
 #	pragma message("Compiler version " DMibStringize(_MSC_VER) "  not supported")
