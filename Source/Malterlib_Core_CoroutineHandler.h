02e6c72227d618da453570dedfc7baf8d94fe0ea
Erik Olofsson <erik@olofsson.info>
2022-03-27 01:24:30 +0100

    [Malterlib/Core] Add support for detecting if resuming with an exception

--- Source/Malterlib_Core_CoroutineHandler.h
+++ Source/Malterlib_Core_CoroutineHandler.h
@@ -53,7 +53,7 @@ namespace NMib
 		void f_Resume() override;
 
 		virtual void f_InitialSuspend() = 0;
-		virtual NFunction::TCFunctionMovable<void ()> f_StoreState(bool _bFromSuspend) = 0;
+		virtual NFunction::TCFunctionMovable<void (bool _bException)> f_StoreState(bool _bFromSuspend) = 0;
 
 		DMibListLinkDS_Link(CCrossActorCallStateScope, m_Link);
 	};
