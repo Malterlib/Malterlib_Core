9806e1ed2fea22e39a20aed01919b0ad3d5d0a66
Erik Olofsson <erik.olofsson@favro.com>
2022-03-28 17:26:31 +0200

    [Malterlib/Core] Fix doxygen

--- Source/Malterlib_Core_CoroutineFlags.h
+++ Source/Malterlib_Core_CoroutineFlags.h
@@ -5,12 +5,13 @@
 
 namespace NMib::NConcurrency
 {
+	/// Specifies options for a coroutine. In debug mode contains properties the coroutine possess.
 	enum ECoroutineFlag
 	{
 		ECoroutineFlag_None = 0
 		, ECoroutineFlag_CaptureExceptions = DMibBit(0)
 		, ECoroutineFlag_AllowReferences = DMibBit(1) 	///< Warning, when you allow references for parameters in your coroutine, make sure that you don't use the reference
-														/// after the first suspension point, as it will be out of scope in this case.
+														///< after the first suspension point, as it will be out of scope in this case.
 		, ECoroutineFlag_BreakSelfReference = DMibBit(2)///< When co_awaiting a future don't hold a reference to the actor
 #if DMibEnableSafeCheck > 0
 		, ECoroutineFlag_UnsafeReferenceParameters = DMibBit(3) // Automatically generated
@@ -18,5 +19,4 @@ namespace NMib::NConcurrency
 #endif
 		, ECoroutineFlag_AllowReferencesCaptureExceptions = ECoroutineFlag_CaptureExceptions | ECoroutineFlag_AllowReferences
 	};
-
 }
