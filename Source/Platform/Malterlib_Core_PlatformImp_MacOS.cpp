6b98f69faf16218b5af1b73a61cedd1ab3a32c29
Erik Olofsson <erik@olofsson.info>
2022-09-25 23:15:12 +0200

    [Malterlib/Thread] Add support for locking low level locks without thread sanitizer checks

--- Source/Platform/Malterlib_Core_PlatformImp_MacOS.cpp
+++ Source/Platform/Malterlib_Core_PlatformImp_MacOS.cpp
@@ -2498,6 +2498,82 @@ namespace NMib::NThread
 
 		DMibSanitizerAnnotate_MutexPostUnlock(this, 0);
 	}
+
+	bool CLowLevelLockAggregate::f_TryLockNoSanitize()
+	{
+#if DPlatformVersion < 10120
+		if (&os_unfair_lock_lock)
+		{
+#endif
+			if (!os_unfair_lock_trylock((os_unfair_lock_t)&m_Lock))
+				return false;
+#if DPlatformVersion < 10120
+		}
+		else
+		{
+			uint32 Expected = 0;
+			if (!m_Lock.f_CompareExchangeStrong(Expected, 1, NAtomic::EMemoryOrder_Acquire, NAtomic::EMemoryOrder_Acquire))
+				return false;
+		}
+#endif
+#ifdef DMibSanitizerEnabled_Thread
+		__tsan_acquire(&m_Lock);
+#endif
+#if DMibEnableSafeCheck > 0
+		m_ThreadID = NSys::fg_Thread_GetCurrentUID();
+		m_AlternateThreadID = NSys::fg_Thread_GetCurrentUIDAlternate();
+#endif
+		return true;
+	}
+
+	void CLowLevelLockAggregate::f_LockNoSanitize()
+	{
+#if DPlatformVersion < 10120
+		if (&os_unfair_lock_lock)
+		{
+#endif
+			os_unfair_lock_lock((os_unfair_lock_t)&m_Lock);
+#if DPlatformVersion < 10120
+		}
+		else
+		{
+			NMib::NThread::CThreadSpinWaiter SpinWaiter;
+			uint32 Expected = 0;
+			while (!m_Lock.f_CompareExchangeStrong(Expected, 1, NAtomic::EMemoryOrder_Acquire, NAtomic::EMemoryOrder_Acquire))
+			{
+				SpinWaiter.f_Wait();
+				Expected = 0;
+			}
+		}
+#endif
+#ifdef DMibSanitizerEnabled_Thread
+		__tsan_acquire(&m_Lock);
+#endif
+#if DMibEnableSafeCheck > 0
+		m_ThreadID = NSys::fg_Thread_GetCurrentUID();
+		m_AlternateThreadID = NSys::fg_Thread_GetCurrentUIDAlternate();
+#endif
+	}
+
+	void CLowLevelLockAggregate::f_UnlockNoSanitize()
+	{
+#if DMibEnableSafeCheck > 0
+		m_ThreadID = 0;
+		m_AlternateThreadID = 0;
+#endif
+#ifdef DMibSanitizerEnabled_Thread
+		__tsan_release(&m_Lock);
+#endif
+#if DPlatformVersion < 10120
+		if (&os_unfair_lock_lock)
+#endif
+			os_unfair_lock_unlock((os_unfair_lock_t)&m_Lock);
+#if DPlatformVersion < 10120
+		else
+			m_Lock.f_Exchange(0, NAtomic::EMemoryOrder_Release);
+#endif
+
+	}
 }
 
 uint16 NSys::fg_Langague_GetSystemLanguage(NMib::NStr::CStr &_Language)
