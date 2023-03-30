d46c50489172cc4f195e9522e16a8e5605282c05
Erik Olofsson <erik@olofsson.info>
2023-03-30 18:37:24 +0200

    [Malterlib/Core] Add Malterlib only exception capture option

--- Source/Malterlib_Core_CoroutineFlags.h
+++ Source/Malterlib_Core_CoroutineFlags.h
@@ -10,12 +10,13 @@ namespace NMib::NConcurrency
 	{
 		ECoroutineFlag_None = 0
 		, ECoroutineFlag_CaptureExceptions = DMibBit(0)
-		, ECoroutineFlag_AllowReferences = DMibBit(1)	///< Warning, when you allow references for parameters in your coroutine, make sure that you don't use the reference
+		, ECoroutineFlag_CaptureMalterlibExceptions = DMibBit(1)
+		, ECoroutineFlag_AllowReferences = DMibBit(2)	///< Warning, when you allow references for parameters in your coroutine, make sure that you don't use the reference
 														///< after the first suspension point, as it will be out of scope in this case.
-		, ECoroutineFlag_BreakSelfReference = DMibBit(2)///< When co_awaiting a future don't hold a reference to the actor
+		, ECoroutineFlag_BreakSelfReference = DMibBit(3)///< When co_awaiting a future don't hold a reference to the actor
 #if DMibEnableSafeCheck > 0
-		, ECoroutineFlag_UnsafeReferenceParameters = DMibBit(3) // Automatically generated
-		, ECoroutineFlag_UnsafeThisPointer = DMibBit(4)			// Automatically generated
+		, ECoroutineFlag_UnsafeReferenceParameters = DMibBit(4) // Automatically generated
+		, ECoroutineFlag_UnsafeThisPointer = DMibBit(5)			// Automatically generated
 #endif
 		, ECoroutineFlag_AllowReferencesCaptureExceptions = ECoroutineFlag_CaptureExceptions | ECoroutineFlag_AllowReferences
 	};
