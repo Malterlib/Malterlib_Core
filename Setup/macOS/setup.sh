a677fbfb363043c3918b7259dea5668e24439361
Erik Olofsson <erik@olofsson.info>
2023-10-08 00:52:27 +0200

    [Malterlib/Core] Add support for Xcode 15.1

--- Setup/macOS/setup.sh
+++ Setup/macOS/setup.sh
@@ -188,7 +188,7 @@ UpdateXcodePlugins()
 	popd > /dev/null
 
 	if $Setting_PatchedLLBuild; then
-		if [[ "$XcodeVersion" == "14.2" ]] || [[ "$XcodeVersion" == "14.3" ]] || [[ "$XcodeVersion" == "14.3.1" ]] || [[ "$XcodeVersion" == "15.0" ]] ; then
+		if [[ "$XcodeVersion" == "14.2" ]] || [[ "$XcodeVersion" == "14.3" ]] || [[ "$XcodeVersion" == "14.3.1" ]] || [[ "$XcodeVersion" == "15.0" ]] || [[ "$XcodeVersion" == "15.1" ]] ; then
 			pushd "$RepositoryDirectory/MalterlibXcodePatches/llbuild" > /dev/null
 
 			if [[ "$HasLibraryValidation" == "false" ]] || [ -e "$2/Contents/SignState.json" ]; then
