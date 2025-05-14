48e3e80a3ebd31a8d733bbf4929d3a1903e02721
Erik Olofsson <erik@olofsson.info>
2025-05-14 11:55:22 +0200

    [Malterlib/Core] Update coloring to use Display P3 color space

--- Test/Test_Malterlib_Core_CodeColoring.cpp
+++ Test/Test_Malterlib_Core_CodeColoring.cpp
@@ -89,3 +89,83 @@ namespace NTest
 
 	};
 }
+
+#if 0
+
+// Language
+= * + - % {} [] () <> // #ffffff
+# // #ffffff
+for if while do // #ffffff
+typename // #c0c0c0
+inline // #c0c0c0
+\ // #808080
+public private protected friend // #ffc8ca
+const volatile // #ffb680
+
+// Builtin types
+int bool void uint32 // #ff5966
+
+// Constant values
+15 60.6 // #ff0080
+t_Test // #ff5bad
+gc_Test ETest_Value mc_Test gc_Test c_Test true false nullptr // #ff8ac5
+mcp_Test // #ca97b1
+tf_Test // #ffb7db
+
+// Character
+'T' // #ff48f0
+
+// Namespace
+NTest // #d785ff
+
+// Types
+t_CTest t_TCTest // #8269ff
+tf_CTest tf_TCTest // #cdc3ff
+CTest TCTest ETest // #b8aaff
+auto // #dbd3ff
+
+// String
+"String" // #009eff
+
+// Functors
+_fTest // #00e4e6
+o_fTest // #36e8cd
+fTest // #00edae
+m_fTest // #00f265
+mp_fTest // #4fc17e
+
+// Functions
+f_Test fs_Test // #26ff00
+fg_Test fsg_Test // #1cb900
+fp_Test fsp_Test  // #8dd580
+
+// Parameters
+_Test p_Test // #e6ff00
+o_Test po_Test // #fff54b
+
+// Variables
+Var // #ffd700
+
+// Concepts
+cComparable // #ffb680
+
+// Member variables
+m_Test // #ffa600
+mp_Test // #c59d53
+
+// Macros
+DTest // #ff7700
+d_Test // #ffbc81
+
+// Globals
+ms_Test // #ff3f1c
+g_Test gs_Test // #e13819
+msp_Test // #d56955
+
+// Comments
+// Comment #003737 #ff9f3b
+/// Documentation comment #003737 #9bcde6
+/// @Keyword #003737 #f070b6
+/// http://example.com #003737 #9dff70
+
+#endif
